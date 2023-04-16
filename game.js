26fe77a403c094f49fb4a0e1394e98ec3f5756e3
teoitaer <129729534+teoitaer@users.noreply.github.com>
2023-04-16 16:05:42 +0200

    Update game.js

--- game.js
+++ game.js
@@ -150,8 +150,8 @@ choices.forEach((choice) => {
 
 incrementScore = (num) => {
     score += num;
-     // scoreText.innerText = (score/x) * 100;
-     scoreText.innerText = 3 * 100;
+      scoreText.innerText = (score/x) * 100;
+
 
 
 };
