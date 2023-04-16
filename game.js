3d31dc36edb8a8a6b5e7aa79682dc791ed589e9c
teoitaer <129729534+teoitaer@users.noreply.github.com>
2023-04-16 16:38:35 +0200

    Update game.js

--- game.js
+++ game.js
@@ -78,7 +78,8 @@ startGame = () => {
 
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
+        // localStorage.setItem('mostRecentScore', score);
+        localStorage.setitem('mostRecentScore', (score/x) * 100);
         //go to the end page
         return window.location.assign('/end.html');
     }
